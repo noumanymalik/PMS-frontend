8f83acf96aaae5b7df64ae42c348c67d161490e4
Nouman Malik <noumanymalik@gmail.com>
2026-08-12 22:37:13 +0500

    Report Triumvirate-Tango-of-Telephony

---
+++ PMS.UI/Models/ViewModels/Report/TriumvirateTangoOfTelephonyReportViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PMS.UI.Models.ViewModels.Report
+{
+    public class TriumvirateTangoOfTelephonyReportViewModel
+    {
+        public int ReportTypeId { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayName("From Date")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM-DD-yyyy}")]
+        public DateTime FromDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayName("To Date")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM-DD-yyyy}")]
+        public DateTime ToDate { get; set; }
+    }
+}
