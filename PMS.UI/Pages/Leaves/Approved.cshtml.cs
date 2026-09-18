42bf4d3846b87448a2e62c4bab0a3ca007d0802e
Nouman Malik <noumanymalik@gmail.com>
2026-09-19 03:18:37 +0500

    Agent-Activity-18-09-2026

---
+++ PMS.UI/Pages/Leaves/Approved.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PMS.UI.Pages.Leaves
+{
+    public class ApprovedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
